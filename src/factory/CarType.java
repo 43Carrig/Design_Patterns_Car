bbd1a4e1cc0bb77d2f0c1b5a534caea1fe7fd8f2
Carrigeendaniel <Sean.Byrne@students.ittralee.ie>
2018-10-27 19:26:36 +0100

    Removed the old CarType class. Refactored enum CarTypeAvailableCars to CarType.

--- src/factory/CarType.java
+++ src/factory/CarType.java
@@ -1,23 +1,8 @@
 package factory;
 
-public abstract class CarType {
-
-    private String type;
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public String getModel() {
-        return "CarType Factory is finished producing an " + this.getType();
-    }
-
-    public String getColor() {
-        return "CarType Factory is painting a " + this.getType();
-    }
+public enum CarType {
+    FORD_FIESTA,
+    FORD_MONDEO,
+    OPEL_ASTRA,
+    OPEL_INSIGNIA
 }
-
--- src/factory/CarTypeAvailableCars.java
+++
@@ -1,8 +0,0 @@
-package factory;
-
-public enum CarTypeAvailableCars {
-    FORD_FIESTA,
-    FORD_MONDEO,
-    OPEL_ASTRA,
-    OPEL_INSIGNIA
-}
