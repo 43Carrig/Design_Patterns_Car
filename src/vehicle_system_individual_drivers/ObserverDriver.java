62263ddf6205cb692f5abc7dbc7e65fba866cfaa
Carrigeendaniel <Sean.Byrne@students.ittralee.ie>
2018-10-30 22:47:19 +0000

    Observer Pattern tidied and tested.

--- src/vehicle_system_individual_drivers/ObserverDriver.java
+++ src/vehicle_system_individual_drivers/ObserverDriver.java
@@ -2,23 +2,13 @@ package vehicle_system_individual_drivers;
 
 import observer.*;
 import observer.LocationObserver;
-import template.Driver;
-import template.NewLearnerDriver;
-import vehicles.Car;
-import vehicles.Fiesta;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class ObserverDriver {
 
     public static void main(String args[])
     {
-//        List<Car> car = new ArrayList<>();
-//
-//        Car fiesta = new Fiesta();
-//        Driver driver = new NewLearnerDriver("Sean");
-
         CarLocation mainCarLocation = new CarLocation(new ArrayList<Observer>());
         LocationObserver carLocationObserver = new LocationObserver(mainCarLocation);
 
