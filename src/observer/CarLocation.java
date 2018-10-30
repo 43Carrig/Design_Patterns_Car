14ebfeb8ce0b9408809c7fdfbe495827ea233fe0
Carrigeendaniel <Sean.Byrne@students.ittralee.ie>
2018-10-30 19:17:47 +0000

    Observer Pattern. CarLocation class constructed with necessary methods and variables.

--- src/observer/CarLocation.java
+++ src/observer/CarLocation.java
@@ -1,18 +1,52 @@
 package observer;
 
+import vehicles.Car;
+
+import java.util.List;
+
 public class CarLocation implements Subject {
+    List<Observer> observers;
+    double longitude;
+    double latitude;
+
+    public CarLocation(List<Observer> observers)
+    {
+        this.observers = observers;
+    }
+
     @Override
     public void register(Observer observer) {
-
+        observers.add(observer);
     }
 
     @Override
     public void unregister(Observer observer) {
-
+        observers.remove(observers);
     }
 
     @Override
     public void notifyObservers() {
+        for(Observer o : observers)
+        {
+            o.update(longitude, latitude);
+        }
+    }
 
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+        notifyObservers();
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+        notifyObservers();
     }
 }
